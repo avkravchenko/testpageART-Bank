0a30ccce0f24aeb02acdb9999b72801b0e11ec79
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-04-14 00:13:39 +0400

    done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const cards = document.querySelectorAll('.main__container__card');
+const extra = document.querySelectorAll('.main__container__card__extra-customer-data');
+
+
+const test0 = cards[0];
+const test1 = extra[0];
+
+console.log(test0)
+console.log(test1)
+
+cards.forEach((card, index) => {
+    card.addEventListener('mouseover', () => {
+        if (index || index === 0) {
+            //extra[index].style.visibility = "visible";
+            extra[index].classList.add('main__container__card__extra-customer-data--active');
+
+        };
+    })
+
+    card.addEventListener('mouseout', () => {
+        if (index || index === 0) {
+            //extra[index].style.visibility = "hidden";
+            extra[index].classList.remove('main__container__card__extra-customer-data--active');
+        }
+    })
+})
